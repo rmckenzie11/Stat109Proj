b664907aadc777f61f60e5a6aa7f1faba62ff572
Dasha Metropolitansky <metropolitansky@college.harvard.edu>
2019-04-28 15:41:09 -0400

    Updated SAT dataframe and removed unnecessary ones

--- scratch.R
+++ scratch.R
@@ -7,16 +7,18 @@ library(lubridate)
 
 sat <- read_excel("2016-17_hs_sqr.xlsx", 
                               sheet = "Additional Info") %>%
-  clean_names()
+  clean_names() %>% 
+  select(dbn, metric_value_average_score_sat_math, 
+         n_count_average_score_sat_math, 
+         metric_value_average_score_sat_reading_and_writing, 
+         n_count_average_score_sat_reading_and_writing)
 
 demographics <- read_excel("demographics.xlsx", 
                            sheet = "School")
-
 demographics <- demographics %>%
   filter(Year == "2016-17",
          `Grade 12` > 0) %>%
   clean_names()
-
 dbn <- demographics$dbn
 dbn2 <- str_sub(dbn, start = -4)
 
@@ -31,24 +33,4 @@ survey_teacher <- read_excel("survey_teacher.xlsx") %>%
 survey_student <- read_excel("survey_student.xlsx") %>%
   filter(DBN %in% dbn) %>%
   clean_names()
-
-quality_review <- read_csv("2005_-_2018_Quality_Review_Ratings.csv") %>%
-  filter(BN %in% dbn2) %>%
-  clean_names()
-
-# Turn school_year into date
-quality_review = quality_review %>% 
-  separate(col = school_year, into = c("school_year", "time"), sep = " ", extra = "merge") %>%
-  mutate(school_year = date(as.POSIXct(school_year, format = "%d/%m/%Y"))) %>%
-  select(-time)
-
-# Turn start_date into date
-quality_review = quality_review %>%
-  separate(col = start_date, into = c("start_date", "time"), sep = " ", extra = "merge") %>%
-  mutate(start_date = date(as.POSIXct(start_date, format = "%m/%d/%Y"))) %>%
-  select(-time)
-
-scores <- read_excel("scores.xlsx", sheet = "Student Achievement") %>%
-  filter(DBN %in% dbn) %>%
-  select(1,2,94,95,96,74,75,76)
 
